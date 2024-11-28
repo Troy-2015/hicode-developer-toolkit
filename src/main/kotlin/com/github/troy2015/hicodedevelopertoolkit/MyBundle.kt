6bb067d48cdc68f6248ee503b3f3d6c485266e5e
GitHub Action <action@github.com>
2024-11-28 11:32:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/troy2015/hicodedevelopertoolkit/MyBundle.kt
+++ src/main/kotlin/com/github/troy2015/hicodedevelopertoolkit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.troy2015.hicodedevelopertoolkit
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/troy2015/hicodedevelopertoolkit/services/MyProjectService.kt
+++ src/main/kotlin/com/github/troy2015/hicodedevelopertoolkit/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.troy2015.hicodedevelopertoolkit.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.troy2015.hicodedevelopertoolkit.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
